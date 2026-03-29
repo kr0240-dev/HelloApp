7b1fa5a32e345127e80950f08c498b3d15aba7f0
kr0240-dev <kr0240@srmist.edu.in>
2026-03-29 18:42:19 +0530

    HelloAppuc2

---
+++ App/src/main/java/org/example/HelloApp2.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class HelloApp2 {
+    public static void main(String[] args){
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+    }
+}
