6a439eb4345216e1de85aad5c33cd71970fc1b4a
kr0240-dev <kr0240@srmist.edu.in>
2026-03-29 18:46:27 +0530

    HelloAppuc3

---
+++ App/src/main/java/org/example/HelloAppuc3.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class HelloAppuc3 {
+    public static void main(String[] args) {
+        String name = "world";
+        if (args.length > 0) {
+            name = args[0];
+        }
+        System.out.println("Hello " + name + "!");
+    }
+}
