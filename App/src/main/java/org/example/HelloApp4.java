bdb016a0db6b6a232efa56c561f753120ab1566b
kr0240-dev <kr0240@srmist.edu.in>
2026-03-29 18:53:40 +0530

    HelloAppuc4

---
+++ App/src/main/java/org/example/HelloApp4.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class HelloApp4 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
+    }
+}
